c5ff0b2328b9d6bf3b8add7c9ec9bc03bc1c61e9
Michael Orji <orjimichael4886@gmail.com>
2023-03-13 22:58:14 +0100

    cleaned up and commented utils

--- src/utils/avatarUrlTagWrapper.js
+++ src/utils/avatarUrlTagWrapper.js
@@ -1,3 +1,4 @@
+//convert avatar url into img tag with src filled. Add user handle as alt text
 function wrapAvatar(url, userHandle) {
     let img = `<img src='${url}' alt='${userHandle}'>`
     return img;
