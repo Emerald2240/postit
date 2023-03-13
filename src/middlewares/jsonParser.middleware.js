a621d5f6c4abf4785d977f6d4918a67e8cc6115e
Michael Orji <orjimichael4886@gmail.com>
2023-03-13 22:27:44 +0100

    cleaned up and commented middlewares

--- src/middlewares/jsonParser.middleware.js
+++ src/middlewares/jsonParser.middleware.js
@@ -1,18 +1,12 @@
+//if the user passes in a bad json request, we return a response rather than let the app crash unexpectedly
 const jsonParserMiddleware = async (err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     // console.error(err);
-    return res.status(400).send({ status: 404, message: err.message }); // Bad request
+    return res.status(400)
+      .send({ status: 404, message: err.message }); // Bad request
   } else {
     next();
   }
 };
 
 module.exports = jsonParserMiddleware;
-
-// app.use((err, req, res, next) => {
-//   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-//       console.error(err);
-//       return res.status(400).send({ status: 404, message: err.message }); // Bad request
-//   }
-//   next();
-// });
